9c6d0cabde10fc70c6ca16a2731da6629c0f1c22
bhangu-creator <bhanguparwindersingh31@gmail.com>
2026-06-21 08:10:58 +0530

    analytics POM

--- tests-e2e/src/tests/workouts/workoutPage.spec.ts
+++ tests-e2e/src/tests/workouts/workoutPage.spec.ts
@@ -3,7 +3,7 @@ import {test,expect} from "@playwright/test";
 import { Workouts } from "../../pages/workoutsPage";
 import { LoginPage } from "../../pages/loginPage";
 import { validUser } from "../../test-data/users";
-import { validWorkout,validChestWorkoutExercises } from "../../test-data/workout";
+import { validWorkout,validChestWorkoutExercises,updateWorkout,updateWorkoutExercises } from "../../test-data/workout";
 
 test.beforeEach('Log in as valid user before crud operations on workouts',async({page})=>
 {
@@ -19,9 +19,37 @@ test.describe('workout crud tests',()=>
         const workout = new Workouts(page);
 
         await workout.createWorkout(validWorkout.workoutTitle,validWorkout.workoutType,validChestWorkoutExercises);
+        await workout.searchWorkout(validWorkout.workoutTitle);
+        await workout.viewWorkout(validWorkout.workoutTitle,validWorkout.workoutType,validChestWorkoutExercises);
+
+        //cleanup
+        await workout.deleteWorkout(validWorkout.workoutTitle);
+
+    })
+
+    test('To verify if user can update a valid Workout',async ({page})=>
+    {
+        const workout = new Workouts(page);
+
+        await workout.createWorkout(validWorkout.workoutTitle,validWorkout.workoutType,validChestWorkoutExercises);
+        await workout.editWorkout(updateWorkout.workoutTitle,updateWorkout.workoutType,updateWorkoutExercises);
+        await workout.searchWorkout(updateWorkout.workoutTitle);
+        await workout.viewWorkout(updateWorkout.workoutTitle,updateWorkout.workoutType,updateWorkoutExercises);
+
+        //cleanup
+        await workout.deleteWorkout(updateWorkout.workoutTitle);
+
+    })
+
+    test('To verify if user can delete the workout successfully',async({page})=>
+    {
+
+        const workout = new Workouts(page);
+
+        await workout.createWorkout(validWorkout.workoutTitle,validWorkout.workoutType,validChestWorkoutExercises);
         await workout.LogWorkoutButton.click();
         await workout.workoutOkModalButton.click();
-        await workout.viewWorkout(validWorkout.workoutTitle,validWorkout.workoutType,validChestWorkoutExercises);
+        await workout.deleteWorkout(validWorkout.workoutTitle);
 
     })
 })
