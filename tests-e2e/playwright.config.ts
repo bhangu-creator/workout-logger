0dac51d90c9dd9f296aa26d667cbbcc43e867f22
bhangu-creator <bhanguparwindersingh31@gmail.com>
2026-06-30 14:48:26 +0530

    config file fix

--- tests-e2e/playwright.config.ts
+++ tests-e2e/playwright.config.ts
@@ -52,17 +52,17 @@ export default defineConfig({
        },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] ,
-      },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] ,
+    //   },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'],
-       },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'],
+    //    },
+    // },
 
     /* Test against mobile viewports. */
     // {
